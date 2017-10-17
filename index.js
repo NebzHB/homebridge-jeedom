31f4885cafe25ee8e600e3866d4938f8fdf5b460
NebzHB <nebz@nebz.be>
2017-10-17 11:18:35 +0200

    if battery === null -> jeedom cache not ready yet -> don't display level low

--- index.js
+++ index.js
@@ -1943,7 +1943,7 @@ JeedomPlatform.prototype.getAccessoryValue = function(characteristic, service) {
 				for (const cmd of cmdList) {
 					if (cmd.generic_type == 'BATTERY' && cmd.id == service.cmd_id) {
 						returnValue = cmd.currentValue;
-						if(returnValue > 20) returnValue = Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
+						if(returnValue===null || returnValue > 20) returnValue = Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
 						else returnValue = Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW;
 						break;
 					}
