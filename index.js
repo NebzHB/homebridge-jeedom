05b96b0dd62601f40bf4ecd5544a6bc9d95b7fc2
Nebz <28622481+NebzHB@users.noreply.github.com>
2022-03-21 11:05:24 +0100

    add Rain and MinimumTemperature

--- index.js
+++ index.js
@@ -2266,6 +2266,16 @@ JeedomPlatform.prototype.AccessoireCreateHomebridge = function(eqLogic) {
 								props.unit=unite;
 								Serv.getCharacteristic(Characteristic.Visibility).setProps(props);
 							}
+						} else if (cmd2.rain) {
+							Serv.infos.rain=cmd2.rain
+							HBservice.characteristics.push(Characteristic.Rain);
+							Serv.addCharacteristic(Characteristic.Rain);
+							Serv.getCharacteristic(Characteristic.Rain).displayName = cmd2.rain.name;
+						} else if (cmd2.temperature_min) {
+							Serv.infos.temperature_min=cmd2.temperature_min
+							HBservice.characteristics.push(Characteristic.MinimumTemperature);
+							Serv.addCharacteristic(Characteristic.MinimumTemperature);
+							Serv.getCharacteristic(Characteristic.MinimumTemperature).displayName = cmd2.temperature_min.name;
 						}
 					});
 
@@ -3780,6 +3790,22 @@ JeedomPlatform.prototype.getAccessoryValue = function(characteristic, service, i
 			case Characteristic.WeatherCondition.UUID :
 				for (const cmd of cmdList) {
 					if (cmd.generic_type == 'WEATHER_CONDITION' && cmd.id == service.infos.condition.id) {
+						returnValue = cmd.currentValue;
+						break;
+					}
+				}
+			break;	
+			case Characteristic.Rain.UUID :
+				for (const cmd of cmdList) {
+					if (cmd.generic_type == 'WEATHER_RAIN' && cmd.id == service.infos.rain.id) {
+						returnValue = cmd.currentValue;
+						break;
+					}
+				}
+			break;	
+			case Characteristic.MinimumTemperature.UUID :
+				for (const cmd of cmdList) {
+					if (cmd.generic_type == 'WEATHER_TEMPERATURE_MIN' && cmd.id == service.infos.temperature_min.id) {
 						returnValue = cmd.currentValue;
 						break;
 					}
@@ -5894,6 +5920,32 @@ function RegisterCustomCharacteristics() {
 	Characteristic.Visibility.UUID = 'd24ecc1e-6fad-4fb5-8137-5af88bd5e857';
 	inherits(Characteristic.Visibility, Characteristic);
 	
+	Characteristic.Rain = function() {
+		Characteristic.call(this, 'Rain', 'F14EB1AD-E000-4EF4-A54F-0CF07B2E7BE7');
+		this.setProps({
+			format: Characteristic.Formats.BOOL,
+			perms: [ Characteristic.Perms.READ, Characteristic.Perms.NOTIFY],
+		});
+		this.value = this.getDefaultValue();
+	};
+	Characteristic.Rain.UUID = 'F14EB1AD-E000-4EF4-A54F-0CF07B2E7BE7';
+	inherits(Characteristic.Rain, Characteristic);
+	
+	Characteristic.MinimumTemperature = function() {
+		Characteristic.call(this, 'MinimumTemperature', '707B78CA-51AB-4DC9-8630-80A58F07E419');
+		this.setProps({
+			format: Characteristic.Formats.FLOAT,
+			unit: Characteristic.Units.CELSIUS,
+			maxValue: 100,
+			minValue: -40,
+			minStep: 0.1,
+			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY],
+		});
+		this.value = this.getDefaultValue();
+	};
+	Characteristic.MinimumTemperature.UUID = '707B78CA-51AB-4DC9-8630-80A58F07E419';
+	inherits(Characteristic.MinimumTemperature, Characteristic);
+	
 	Characteristic.NoiseLevel = function() {
 			Characteristic.call(this, 'Noise Level', 'b3bbfabc-d78c-5b8d-948c-5dac1ee2cde5');
 			this.setProps({
@@ -6032,6 +6084,8 @@ function RegisterCustomCharacteristics() {
 		this.addOptionalCharacteristic(Characteristic.WindSpeed);
 		// this.addOptionalCharacteristic(Characteristic.WeatherCondition);
 		this.addOptionalCharacteristic(Characteristic.UVIndex);
+		this.addOptionalCharacteristic(Characteristic.Rain);
+		this.addOptionalCharacteristic(Characteristic.MinimumTemperature);
 	};
 	inherits(Service.WeatherService, Service);
 	Service.WeatherService.UUID = 'E863F001-079E-48FF-8F27-9C2605A29F52';	
